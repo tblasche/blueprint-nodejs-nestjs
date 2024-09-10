b234f5e66194995d727ef228f6084f812515027d
Torsten <tbcollect.github@web.de>
2024-09-10 22:25:21 +0200

    removed dead code

--- src/infrastructure/db/prisma.service.ts
+++ src/infrastructure/db/prisma.service.ts
@@ -2,14 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  // constructor(configService: ConfigService) {
-  //   super({
-  //     datasources: {
-  //       db: {
-  //         url: configService.get<string>('DATABASE_URL')
-  //       }
-  //     }
-  //   });
-  // }
-}
+export class PrismaService extends PrismaClient {}
